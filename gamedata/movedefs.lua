88bc189fce9e2844c311a6dc66e2fe3cb3db4b4b
s44-yuritch <s44-yuritch@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2008-11-22 22:27:02 +0000

    Added US LCS(L) model. Added the unit to the ship test branch. Made the ship test actually work with the new S'44

---
+++ gamedata/movedefs.lua
@@ -0,0 +1,103 @@
+local moveDefs 	=	 {
+	{
+		name					=	"KBOT_Infantry",
+		footprintX		=	1,
+		maxWaterDepth	=	10,
+		maxSlope			=	30,
+	},
+	{
+		name					=	"TANK_Truck",
+		footprintX		=	3,
+		maxWaterDepth	=	5,
+		maxSlope			=	17,
+	},
+	{
+		name					=	"TANK_Car",
+		footprintX		=	2,
+		maxWaterDepth	=	8,
+		maxSlope			=	18,
+	},
+	{
+		name					=	"TANK_Light",
+		footprintX		=	2,
+		maxWaterDepth	=	8,
+		maxSlope			=	22,
+	},
+	{
+		name					=	"TANK_Medium",
+		footprintX		=	3,
+		maxWaterDepth	=	10,
+		maxSlope			=	21,
+	},
+	{
+		name					=	"TANK_Heavy",
+		footprintX		=	3,
+		maxWaterDepth	=	15,
+		maxSlope			=	20,
+	},
+	{
+		name					=	"TANK_SuperHeavy",
+		footprintX		=	4,
+		maxWaterDepth	=	15,
+		maxSlope			=	18,
+	},
+	{
+		name					=	"KBOT_Gun",
+		footprintX		=	2,
+		maxWaterDepth	=	5,
+		maxSlope			=	24,
+	},
+	{
+		name					=	"BOAT_Small",
+		footprintX		=	3,
+		minWaterDepth	=	5,
+		crushStrength	=	10,
+	},
+	{
+		name					=	"BOAT_Medium",
+		footprintX		=	15,
+		minWaterDepth	=	5,
+		crushStrength	=	10,
+	},
+	{
+		name					=	"BOAT_Monitor",
+		footprintX		=	8,
+		minWaterDepth	=	6,
+		crushStrength	=	10,
+	},
+	{
+		name					=	"BOAT_LightPatrol",
+		footprintX		=	8,
+		minWaterDepth	=	10,
+		crushStrength	=	10,
+	},
+	{
+		name					=	"BOAT_Destroyer",
+		footprintX		=	8,
+		minWaterDepth	=	20,
+		crushStrength	=	10,
+	},
+	{
+		name					=	"BOAT_LandingCraft",
+		footprintX		=	8,
+		minWaterDepth	=	2,
+		crushStrength	=	10,
+	},
+	{
+		name					=	"HOVER_AmphibTruck",
+		footprintX		=	3,
+		footprintY		=	3,
+		MaxSlope		=	17,
+		MaxWaterSlope		=	255,
+		crushStrength		=	10,
+	},
+	{
+		name					=	"BOAT_Sub",
+		footprintX		=	8,
+		minWaterDepth	=	40,
+		crushStrength	=	10,
+		submarine	=	1,
+	}
+}
+
+return moveDefs
