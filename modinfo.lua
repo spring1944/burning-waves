4fbbec4c89dae44c55eeebb2e6da1085c0e992cc
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-08-24 02:12:07 +0000

    Fixing modinfo for s44ships so it can be used as a testing ground.

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+local modinfo = {
+		name					=	"BURNING WAVES: TERRORS ON THE HIGH SEAS",
+		shortName				= "S44",
+		game					= "Spring 1944",
+		shortGame				= "S44",
+		mutator					= "Official",
+		description				=	"BURNING WAVES: TERRORS ON THE HIGH SEAS",
+		url						=	"http://www.spring1944.net/",
+		modtype					=	"1",
+		depend					=	{"Spring: 1944 $VERSION"},
+}
+
+return modinfo
