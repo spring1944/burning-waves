42984c973617696e49e94f471b58733904bce745
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2010-08-24 02:39:21 +0000

    and rebirth (with just US stuff, for now).

---
+++ gamedata/messages.lua
@@ -0,0 +1,10 @@
+local messages = {
+	["Team%i(%s) is no more"] = {
+		"Team%i(%s) was not Jack Sparrow.",
+		"Team%i(%s) sank.",
+		"Team%i(%s) is sharkbait.",
+		"Team%i(%s) preferred the lobster.",
+	}
+}
+
+return messages
