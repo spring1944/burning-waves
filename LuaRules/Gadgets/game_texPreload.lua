e5eb4336c6370f4f973e7a364b71617782f6c7ce
NemoNobody <NemoNobody@23f83f84-747b-46a2-a8e3-a4a6079cc213>
2011-10-15 05:29:52 +0000

    Fixing S44 ships to run (updated dependency). Also committing random files in my copy. Probably a good idea?

---
+++ LuaRules/Gadgets/game_texPreload.lua
@@ -0,0 +1,43 @@
+function gadget:GetInfo()
+	return {
+		name      = "Texture preloader",
+		desc      = "Loads select models/skins during the pre-game time instead of at game start",
+		author    = "Gnome, FLOZi",
+		date      = "October 2008",
+		license   = "GNU GPL v2",
+		layer     = -math.huge,
+		enabled   = false
+}
+end
+
+if(gadgetHandler:IsSyncedCode()) then
+-- 	SYNCED
+
+else
+--	UNSYNCED
+local hqDefs = VFS.Include("LuaRules/Configs/hq_spawn.lua")
+local sides = {"gbr", "ger", "us", "rus"} -- this is unpleasant
+
+local function PreloadUnitTexture(unitName)
+	gl.PushMatrix()
+		gl.Translate(100, -1000, 100)
+		gl.Color(1,1,1,0)
+		gl.UnitShape(UnitDefNames[unitName].id,0)
+		gl.Color(1,1,1,1)
+	gl.PopMatrix()
+end
+
+function gadget:DrawWorld()
+	PreloadUnitTexture("flag")
+	for _, side in pairs(sides) do
+		local startUnit = Spring.GetSideData(side)
+		local spawnList = hqDefs[startUnit]
+		for i = 1, #spawnList.units do
+			local unitName = spawnList.units[i]
+			PreloadUnitTexture(unitName)
+		end
+	end
+	gadgetHandler:RemoveGadget()
+end
+
+end
